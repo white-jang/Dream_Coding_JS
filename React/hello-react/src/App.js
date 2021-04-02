ebd951b8f7f0c06a45177cde16eed511237c2b9e
white-jang <galaxy__12@naver.com>
2021-04-02 15:45:39 +0900

    ~15chap

--- React/hello-react/src/App.js
+++ React/hello-react/src/App.js
@@ -1,35 +1,90 @@
-import React, {useRef} from "react";
+import React, { useRef, useState } from "react";
+import CreateUser from "./CreateUser";
 import UserList from "./UserList";
 
 function App() {
-    const users = [
-      {
-        id: 1,
-        username: 'velopert',
-        email: 'public.velopert@gmail.com'
-      },
-      {
-        id: 2,
-        username: 'tester',
-        email: 'tester@example.com'
-      },
-      {
-        id: 3,
-        username: 'liz',
-        email: 'liz@example.com'
-      }
-    ];
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+  });
 
-    // useRef로 관리하는 변수는 값이 바뀐다고 해서 컴포넌트가 리렌더링되지 않음
-    // => 컴포넌트가 리렌더링 되어도 그 값이 계속 기억된다
-    const nextId = useRef(4);
+  const { username, email } = inputs;
 
-    const onCreate = () => {
-  
-      nextId.current += 1;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  // 배열도 불변성을 유지해야 한다.
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+      active: true,
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+      active: false,
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+      active: false,
+    },
+  ]);
+
+  // useRef로 관리하는 변수는 값이 바뀐다고 해서 컴포넌트가 리렌더링되지 않음
+  // => 컴포넌트가 리렌더링 되어도 그 값이 계속 기억된다
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
     };
+    // 기존의 배열은 복사해서 넣어 불변성을 유지하면서 새로운 항목 추가
+    // ... 연산자나 concat 활용
+    // setUsers(users.concat(user));
+    setUsers([...users, user]);
+    setInputs({
+      username: "",
+      email: "",
+    });
+    nextId.current += 1;
+  };
 
-    return <UserList users={users}/>;
+  const onRemove = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
+  // 배열의 특정 요소만 업데이트하기
+  const onToggle = (id) => {
+    setUsers(
+      users.map((user) =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
+
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+    </>
+  );
 }
 
 export default App;
--- React/hello-react/src/UserList.js
+++ React/hello-react/src/UserList.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React from "react";
 
-function User({user}) {
-    return ( 
-        <div>
-          <b>{user.username}</b> <span>({user.email})</span>
-        </div>);
-}
-
-function UserList({users}) {
+function User({ user, onRemove, onToggle }) {
+  const { username, email, id } = user;
   return (
     <div>
-        {
-            // 웬만하면 key는 인덱스를 사용해서 만들지 않는 것이 좋다
-            users.map(user => <User user={user} key={user.id}/>)
-        }
+      <b
+        style={{ color: user.active ? "green" : "black", cursor: "pointer" }}
+        onClick={() => onToggle(id)}
+      >
+        {username}
+      </b>
+      &nbsp;
+      <span>({email})</span>
+      <button onClick={() => onRemove(id)}>삭제</button>
+    </div>
+  );
+}
+
+function UserList({ users, onRemove, onToggle }) {
+  return (
+    <div>
+      {
+        // 웬만하면 key는 인덱스를 사용해서 만들지 않는 것이 좋다
+        users.map((user) => (
+          <User
+            user={user}
+            key={user.id}
+            onRemove={onRemove}
+            onToggle={onToggle}
+          />
+        ))
+      }
     </div>
   );
 }
