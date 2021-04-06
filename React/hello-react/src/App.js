c000308eb43e089003d81165efaec482e6b10cd8
white-jang <galaxy__12@naver.com>
2021-04-06 17:01:05 +0900

    ~18chap

--- React/hello-react/src/App.js
+++ React/hello-react/src/App.js
@@ -1,6 +1,11 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useMemo, useCallback } from "react";
 import CreateUser from "./CreateUser";
 import UserList from "./UserList";
+
+function countActiveUsers(users) {
+  console.log("활성 사용자 수를 세는 중...");
+  return users.filter((user) => user.active).length;
+}
 
 function App() {
   const [inputs, setInputs] = useState({
@@ -10,13 +15,16 @@ function App() {
 
   const { username, email } = inputs;
 
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value,
+      });
+    },
+    [inputs]
+  );
 
   // 배열도 불변성을 유지해야 한다.
   const [users, setUsers] = useState([
@@ -44,7 +52,7 @@ function App() {
   // => 컴포넌트가 리렌더링 되어도 그 값이 계속 기억된다
   const nextId = useRef(4);
 
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
@@ -52,27 +60,33 @@ function App() {
     };
     // 기존의 배열은 복사해서 넣어 불변성을 유지하면서 새로운 항목 추가
     // ... 연산자나 concat 활용
+    // setUsers([...users, user])
     // setUsers(users.concat(user));
-    setUsers([...users, user]);
+    // user 값이 바뀔 때마다 리렌더링되는 것을 방지하기 위해 user 값을 바로 받아오는 게 아니라
+    // setUsers 함수를 이용해 설정
+    setUsers((users) => users.concat(user));
     setInputs({
       username: "",
       email: "",
     });
     nextId.current += 1;
-  };
+  }, [username, email]);
 
-  const onRemove = (id) => {
-    setUsers(users.filter((user) => user.id !== id));
-  };
+  const onRemove = useCallback((id) => {
+    // 컴포넌트가 처음 렌더링될 때 딱 한 번 만들어지고 계속해서 재사용됨
+    setUsers((users) => users.filter((user) => user.id !== id));
+  }, []);
 
   // 배열의 특정 요소만 업데이트하기
-  const onToggle = (id) => {
-    setUsers(
+  const onToggle = useCallback((id) => {
+    setUsers((users) =>
       users.map((user) =>
         user.id === id ? { ...user, active: !user.active } : user
       )
     );
-  };
+  }, []);
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
     <>
@@ -83,6 +97,7 @@ function App() {
         onCreate={onCreate}
       />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성 사용자 수 : {count}</div>
     </>
   );
 }
--- React/hello-react/src/UserList.js
+++ React/hello-react/src/UserList.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function User({ user, onRemove, onToggle }) {
+const User = React.memo(function User({ user, onRemove, onToggle }) {
   const { username, email, id } = user;
+
+  // useEffect(() => {
+  //   // deps가 빈 배열이면 컴포넌트가 마운트될 때 실행
+  //   console.log("컴포넌트가 화면에 나타남.");
+  //   return () => {
+  //     // cleanup 함수. 컴포넌트가 언마운트될 때 실행
+  //     console.log("컴포넌트가 화면에서 사라짐");
+  //   };
+  // }, []);
+
+  useEffect(() => {
+    // user 값이 설정되거나(=마운트될 때) 업데이트될 때마다 실행
+    console.log(user);
+  }, [user]);
+
   return (
     <div>
       <b
@@ -15,7 +30,7 @@ function User({ user, onRemove, onToggle }) {
       <button onClick={() => onRemove(id)}>삭제</button>
     </div>
   );
-}
+});
 
 function UserList({ users, onRemove, onToggle }) {
   return (
@@ -35,4 +50,4 @@ function UserList({ users, onRemove, onToggle }) {
   );
 }
 
-export default UserList;
+export default React.memo(UserList);
