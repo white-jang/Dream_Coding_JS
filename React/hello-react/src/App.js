3fa65d0de16679981688c4114dadfbb82f8e5ef6
white-jang <galaxy__12@naver.com>
2021-04-08 00:02:05 +0900

    ~23chap

--- React/hello-react/src/App.js
+++ React/hello-react/src/App.js
@@ -1,33 +1,16 @@
-import React, { useRef, useState, useMemo, useCallback } from "react";
+import React, { useRef, useReducer, useMemo, useCallback } from "react";
 import CreateUser from "./CreateUser";
 import UserList from "./UserList";
+import useInputs from "./useInputs";
 
 function countActiveUsers(users) {
   console.log("활성 사용자 수를 세는 중...");
   return users.filter((user) => user.active).length;
 }
 
-function App() {
-  const [inputs, setInputs] = useState({
-    name: "",
-    email: "",
-  });
-
-  const { username, email } = inputs;
-
-  const onChange = useCallback(
-    (e) => {
-      const { name, value } = e.target;
-      setInputs({
-        ...inputs,
-        [name]: value,
-      });
-    },
-    [inputs]
-  );
-
-  // 배열도 불변성을 유지해야 한다.
-  const [users, setUsers] = useState([
+// 앱 컴포넌트에서 사용할 초기 상태를 앱 컴포넌트 밖에 선언
+const initialState = {
+  users: [
     {
       id: 1,
       username: "velopert",
@@ -46,44 +29,68 @@ function App() {
       email: "liz@example.com",
       active: false,
     },
-  ]);
+  ],
+};
 
-  // useRef로 관리하는 변수는 값이 바뀐다고 해서 컴포넌트가 리렌더링되지 않음
-  // => 컴포넌트가 리렌더링 되어도 그 값이 계속 기억된다
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE_USER":
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user),
+      };
+    case "TOGGLE_USER":
+      return {
+        ...state,
+        users: state.users.map((user) =>
+          user.id === action.id ? { ...user, active: !user.active } : user
+        ),
+      };
+    case "REMOVE_USER":
+      return {
+        ...state,
+        users: state.users.filter((user) => user.id !== action.id),
+      };
+    default:
+      throw new Error("Unhandled action");
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [form, onChange, reset] = useInputs({
+    username: "",
+    email: "",
+  });
+  const { username, email } = form;
   const nextId = useRef(4);
+  const { users } = state;
 
   const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email,
-    };
-    // 기존의 배열은 복사해서 넣어 불변성을 유지하면서 새로운 항목 추가
-    // ... 연산자나 concat 활용
-    // setUsers([...users, user])
-    // setUsers(users.concat(user));
-    // user 값이 바뀔 때마다 리렌더링되는 것을 방지하기 위해 user 값을 바로 받아오는 게 아니라
-    // setUsers 함수를 이용해 설정
-    setUsers((users) => users.concat(user));
-    setInputs({
-      username: "",
-      email: "",
+    dispatch({
+      type: "CREATE_USER",
+      user: {
+        id: nextId.current,
+        username,
+        email,
+      },
     });
     nextId.current += 1;
-  }, [username, email]);
+    reset();
+  }, [username, email, reset]);
 
-  const onRemove = useCallback((id) => {
-    // 컴포넌트가 처음 렌더링될 때 딱 한 번 만들어지고 계속해서 재사용됨
-    setUsers((users) => users.filter((user) => user.id !== id));
+  const onToggle = useCallback((id) => {
+    dispatch({
+      type: "TOGGLE_USER",
+      id,
+    });
   }, []);
 
-  // 배열의 특정 요소만 업데이트하기
-  const onToggle = useCallback((id) => {
-    setUsers((users) =>
-      users.map((user) =>
-        user.id === id ? { ...user, active: !user.active } : user
-      )
-    );
+  const onRemove = useCallback((id) => {
+    dispatch({
+      type: "REMOVE_USER",
+      id,
+    });
   }, []);
 
   const count = useMemo(() => countActiveUsers(users), [users]);
@@ -96,8 +103,8 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
-      <div>활성 사용자 수 : {count}</div>
+      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
+      <div>활성 사용자 수 : {count} </div>
     </>
   );
 }
--- React/hello-react/src/Counter.js
+++ React/hello-react/src/Counter.js
@@ -1,27 +1,36 @@
-import React, {useState} from "react";
+import React, { useReducer } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      throw new Error("Unhandled action");
+  }
+}
 
 function Counter() {
-    const [number, setNumber] = useState(0);
-    const onIncrease = () => {
-        // setNumber(number + 1);
-        // 직접적으로 number값에 접근하는 것보다 업데이트 함수를 이용하는게 최적화에 도움이 된다
-        // prevNumber가 아니라 어떤 이름이어도 상관 없다
-        setNumber((prevNumber) => prevNumber + 1); // 함수형 업데이트
-    };
+  const [number, dispatch] = useReducer(reducer, 0);
 
-    const onDecrease = () => {
-        setNumber((prevNumber) => prevNumber - 1);
-    };
+  const onIncrease = () => {
+    dispatch({ type: "INCREMENT" });
+  };
 
-    return (
-        <div>
-            <h1>{number}</h1>
-            <button onClick={onIncrease}>+1</button>
-            {/* 함수를 호출하는 게 아니라 (onIncrease()) 함수 이름을 써줘야 함 */}
-            {/* 함수를 호출하듯이 하면 렌더링될 때 호출되버림... */}
-            <button onClick={onDecrease}>-1</button>
-        </div>
-    );
+  const onDecrease = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+
+  return (
+    <div>
+      <h1>{number}</h1>
+      <button onClick={onIncrease}>+1</button>
+      {/* 함수를 호출하는 게 아니라 (onIncrease()) 함수 이름을 써줘야 함 */}
+      {/* 함수를 호출하듯이 하면 렌더링될 때 호출되버림... */}
+      <button onClick={onDecrease}>-1</button>
+    </div>
+  );
 }
 
 export default Counter;
